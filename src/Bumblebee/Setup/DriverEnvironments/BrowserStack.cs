721556d1ae422b23d43d4201a96421a47009baec
Brendan O'Rourke <brendan@optimizely.com>
2016-07-29 07:26:16 -0700

    Spacing

--- src/Bumblebee/Setup/DriverEnvironments/BrowserStack.cs
+++ src/Bumblebee/Setup/DriverEnvironments/BrowserStack.cs
@@ -8,18 +8,18 @@ namespace Bumblebee.Setup.DriverEnvironments
 	public class BrowserStack<TWebDriver> : RemoteDriverEnvironment<TWebDriver>
 		where TWebDriver : IWebDriver, new()
     {
-		public string bsUserName { get; set; }
-		public string bsKey { get; set; }
+	public string bsUserName { get; set; }
+	public string bsKey { get; set; }
 
 		public BrowserStack(DesiredCapabilities capabilities) : base(capabilities)
-        {
-            this.remoteURL = "http://hub-cloud.browserstack.com/wd/hub/";
+		{
+			this.remoteURL = "http://hub-cloud.browserstack.com/wd/hub/";
 
-            this.bsUserName = Environment.GetEnvironmentVariable("BROWSERSTACK_USERNAME");
-            this.bsKey = Environment.GetEnvironmentVariable("BROWSERSTACK_KEY");
+			this.bsUserName = Environment.GetEnvironmentVariable("BROWSERSTACK_USERNAME");
+			this.bsKey = Environment.GetEnvironmentVariable("BROWSERSTACK_KEY");
 
-            this.capabilities.SetCapability("browserstack.user", bsUserName);
-            this.capabilities.SetCapability("browserstack.key", bsKey);
-        }
-    }
+			this.capabilities.SetCapability("browserstack.user", bsUserName);
+			this.capabilities.SetCapability("browserstack.key", bsKey);
+		}
+	}
 }
